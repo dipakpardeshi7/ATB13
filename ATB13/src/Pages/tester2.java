0b903279e91e17cb1bf7ca86d1acb652050b3eba
dipakpardeshi7 <dipakpardeshi7@gmail.com>
2025-05-17 17:24:33 +0530

    added tester2 class and updated the tester class

--- ATB13/src/Pages/tester2.java
+++ ATB13/src/Pages/tester2.java
@@ -1,11 +1,11 @@
 package Pages;
 
-public class Tester {
+public class tester2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         System.out.println("Hell0");
-         System.out.println("Hell0");
+		  System.out.println("Hell0");
+	         System.out.println("Hell0");
 	}
 
 }
